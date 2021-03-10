49abe6ce177c51c6545447851fa05f0fbcd73aa4
caiocrocha <caiocedrola@ice.ufjf.br>
2021-03-09 23:12:41 -0300

    Correcoes de erros de arquivo

--- gauss.m
+++ gauss.m
@@ -1,18 +1,19 @@
-#!/snap/bin/octave.octave-cli -qf
+#!/snap/bin/octave.octave-cli
 1;
-function gauss(f, a, b, n)
+function integra(f, a, b, n)
     w = zeros(n, 1);
     t = zeros(n, 1);
     for i = 1:n/2,
-        w[i] = i*(b-a)/(2*n);
-        t[i] = a + i*w[i]/2;
+        w(i) = i*(b-a)/(2*n);
+        t(i) = a + i*w(i)/2;
+        printf("%.4f %.4f\n", w(i), t(i));
     endfor
 endfunction
 
 addpath(pwd);
 args = argv();
 
-f = @(x) sqrt(1 + x.^2);
-gauss(@f, 0, 1, 10);
+f = @(x) sqrt(1 + x.^2)
+integra(f, 0, 1, 10);
 
 printf("Fim do programa\n");
