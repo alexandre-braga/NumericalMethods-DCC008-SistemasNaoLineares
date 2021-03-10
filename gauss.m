8c8dc634a64213131efcf647a1e2c0cced2bd151
caiocrocha <caiocedrola@ice.ufjf.br>
2021-03-10 00:19:40 -0300

    Metodo de Simpson 3/8 repetido para 3N

--- gauss.m
+++ gauss.m
@@ -3,17 +3,19 @@
 function integra(f, a, b, n)
     w = zeros(n, 1);
     t = zeros(n, 1);
+    printf("%4s    %4s    %4s\n", "w", "t", "f(t)");
     for i = 1:n/2,
         w(i) = i*(b-a)/(2*n);
         t(i) = a + i*w(i)/2;
-        printf("%.4f %.4f\n", w(i), t(i));
+        printf("%.4f %.4f %.4f\n", w(i), t(i), f(t(i)));
     endfor
 endfunction
 
 addpath(pwd);
 args = argv();
 
-f = @(x) sqrt(1 + x.^2)
-integra(f, 0, 1, 10);
+f = @(x) 1/x;
+I = simpson38(f, 1, 2, 6);
+printf("I = %f\n", I);
 
 printf("Fim do programa\n");
