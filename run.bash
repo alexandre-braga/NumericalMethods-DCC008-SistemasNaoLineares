54573c12f4f064a7a19019198178e40ad7bbafae
caiocrocha <caiocedrola@ice.ufjf.br>
2021-03-13 00:42:42 -0300

    Intervalo -3 a 3

--- run.bash
+++ run.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+n=$1
+
+mkdir "Dom${n}_${n}"
+
 for i in {1..7} ; do
-    octave-cli gauss.m && mv "PesosEPontosIntegrecaoFinal.txt" "Dom2_2/${i}_PesosEPontosIntegrecaoFinal.txt"
+    octave-cli gauss.m && mv "PesosEPontosIntegrecaoFinal.txt" "Dom${n}_${n}/${i}_PesosEPontosIntegrecaoFinal.txt"
 done
