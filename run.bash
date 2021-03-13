960c7d815979d68f59c36db042a620a603c213bb
caiocrocha <caiocedrola@ice.ufjf.br>
2021-03-13 00:52:42 -0300

    Integracao de -3 a 1

--- run.bash
+++ run.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-n=$1
+a=$1
+b=$2
 
-mkdir "Dom${n}_${n}"
+mkdir "Dom${a##*[+-]}_${b##*[+-]}"
 
 for i in {1..7} ; do
-    octave-cli gauss.m && mv "PesosEPontosIntegrecaoFinal.txt" "Dom${n}_${n}/${i}_PesosEPontosIntegrecaoFinal.txt"
+    octave-cli gauss.m && mv "PesosEPontosIntegrecaoFinal.txt" "Dom${a##*[+-]}_${b##*[+-]}/${i}_PesosEPontosIntegrecaoFinal.txt"
 done
